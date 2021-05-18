dafde41fb80996121636d3c0d829d651530a669a
takuma nishioka <takuma19781192@gmail.com>
2021-05-18 09:08:31 +0900

    test

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\todo;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = todo::all();
+        return response()->json([
+            'message' => 'OK',
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new todo();
+        $item->todo = $request->todo;
+        $item->email = $request->email;
+        $item->save();
+        return response()->json([
+            'message' => 'Created successfully',
+            'data' => $item
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, todo $todo)
+    {
+        $item = todo::where('id', $todo)->first();
+        $item->todo = $request->todo;
+        $item->save();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(todo $todo)
+    {
+        $item = todo::where('id', $todo->id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => $item,
+            ], 404);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+Route::apiResource('/todo', TodoController::class);
